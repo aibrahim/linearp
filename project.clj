a008e50c4c918d1694ccdc41805657fb2a68a850
Abdullah Ibrahim <visof@localhost.localdomain>
2019-03-19 00:24:38 +0200

    fix url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject linearp "0.1.0-SNAPSHOT"
   :description "simple linear programming solver based on clp-java lib."
-  :url ""
+  :url "https://github.com/aibrahim/linearp"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.9.0"]
