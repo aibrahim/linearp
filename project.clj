cadf3e33b9a2e97ff1c73765ae3dc974d78a384d
Abdullah Ibrahim <abdullahibra@gmail.com>
2019-03-19 11:46:54 +0200

    update license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject linearp "0.1.0-SNAPSHOT"
   :description "simple linear programming solver based on clp-java lib."
   :url "https://github.com/aibrahim/linearp"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "Eclipse Public License"
+            :url "https://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [com.quantego/clp-java "1.16.10"]]
   :target-path "target/%s"
